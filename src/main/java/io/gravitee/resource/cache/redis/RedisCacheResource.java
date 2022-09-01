1cd894ff48ae41adf8569323c77fd981089097e7
Jeoffrey Haeyaert <jeoffrey.haeyaert@gmail.com>
2022-09-01 10:02:06 +0200

    feat: improve execution context structure
    gravitee-io/issues#8386

--- src/main/java/io/gravitee/resource/cache/redis/RedisCacheResource.java
+++ src/main/java/io/gravitee/resource/cache/redis/RedisCacheResource.java
@@ -17,7 +17,7 @@ package io.gravitee.resource.cache.redis;
 
 import static java.lang.Boolean.TRUE;
 
-import io.gravitee.gateway.jupiter.api.context.ExecutionContext;
+import io.gravitee.gateway.jupiter.api.context.GenericExecutionContext;
 import io.gravitee.resource.cache.api.Cache;
 import io.gravitee.resource.cache.api.CacheResource;
 import io.gravitee.resource.cache.redis.configuration.HostAndPort;
@@ -72,7 +72,7 @@ public class RedisCacheResource extends CacheResource<RedisCacheResourceConfigur
     }
 
     @Override
-    public Cache getCache(ExecutionContext ctx) {
+    public Cache getCache(GenericExecutionContext ctx) {
         return getCache(ctx.getAttributes());
     }
 
